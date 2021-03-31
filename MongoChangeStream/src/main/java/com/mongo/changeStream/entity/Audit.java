c76e2e492219ee105a7c94edf31fa0d19205bbe8
Raju Rayappa <reddyraja86@gmail.com>
2021-03-31 21:04:56 +0530

    Update Audit.java

--- MongoChangeStream/src/main/java/com/mongo/changeStream/entity/Audit.java
+++ MongoChangeStream/src/main/java/com/mongo/changeStream/entity/Audit.java
@@ -1,29 +1,64 @@
 package com.mongo.changeStream.entity;
 
+import org.bson.BsonDocument;
 import org.springframework.data.annotation.Id;
-
 
 public class Audit {
 
 	@Id
 	public String id;
 
-	public String firstName;
-	public String lastName;
+	private String firstName;
+	private String lastName;
 
-	public Audit() {}
+	private BsonDocument resumeToken;
 
-	public Audit(String firstName, String lastName) {
+	public BsonDocument getResumeToken() {
+		return resumeToken;
+	}
+
+	public void setResumeToken(BsonDocument resumeToken) {
+		this.resumeToken = resumeToken;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	
+	
+	public Audit() {
+	}
+
+	public Audit(String firstName, String lastName,BsonDocument resumeToken) {
 		this.firstName = firstName;
 		this.lastName = lastName;
+		this.resumeToken = resumeToken;
 	}
 
 	@Override
 	public String toString() {
-		return String.format(
-				"Customer[id=%s, firstName='%s', lastName='%s']",
-				id, firstName, lastName);
+		return String.format("Customer[id=%s, firstName='%s', lastName='%s']", id, firstName, lastName);
 	}
 
 }
-
